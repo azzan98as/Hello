fd2d6ed5876577d576c1b370ffef05d03b2310cd
azzan98as <azzan99as@gmail.com>
2019-01-17 11:27:55 -0700

    Create hello.c

---
+++ hello.c/hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Azan Alshukaili and hyden
+ * Date: 2018/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+Azan Alshukaili and hyden
+int main(int argc, char **argv) {
+
+  printf("Hello Azan!\n");
+
+  return 0;
+}
